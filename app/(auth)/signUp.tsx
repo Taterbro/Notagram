7fad2cff05425abc7975692d5360efcc9ef0afb8
Taterbro <135230103+Taterbro@users.noreply.github.com>
2025-11-30 22:23:24 +0100

    signup page

--- app/(auth)/signUp.tsx
+++ app/(auth)/signUp.tsx
@@ -1,16 +1,85 @@
+import CustomInput from "@/components/RHF/customInput";
 import { typography } from "@/constants/theme";
 import { SafeAreaWrapper } from "@/hoc/SafeAreaWrapper";
 import { useTheme } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { useForm, useWatch } from "react-hook-form";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button, Dialog, Portal } from "react-native-paper";
 
 export default function Signup() {
   const { colors } = useTheme();
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const initialPassword = useWatch({ name: "password", control: control });
+  const [isDialogOpen, setDialogOpen] = useState(false);
   const styles = createStyles(colors);
+
+  const onSubmit = (data: any) => {
+    console.log("yuppesr");
+    setDialogOpen(true);
+  };
+  useEffect(() => {
+    console.log("init pass: ", initialPassword);
+  }, [initialPassword]);
   return (
     <SafeAreaWrapper style={{ justifyContent: "center" }}>
-      <View>
+      <View style={{ flex: 1 }}>
         <Text style={styles.title}>Create an account</Text>
+        <ScrollView style={{ gap: 8, height: "50%" }}>
+          <CustomInput
+            required={true}
+            name="email"
+            control={control}
+            label="Email"
+            isEmail={true}
+          />
+          <CustomInput
+            required={true}
+            name="password"
+            control={control}
+            label="Password"
+            isPassword={true}
+          />
+          <CustomInput
+            required={true}
+            name="confirmPassword"
+            control={control}
+            label="Confirm Password"
+            isPassword={true}
+            rules={{
+              validate: (value) =>
+                value != initialPassword ? "Passwords do not match" : undefined,
+            }}
+          />
+        </ScrollView>
+        <Button mode="contained" onPress={handleSubmit(onSubmit)}>
+          <Text>Submit</Text>
+        </Button>
       </View>
+
+      <Portal>
+        <Dialog
+          style={{ backgroundColor: colors.background }}
+          visible={isDialogOpen}
+          onDismiss={() => setDialogOpen(false)}
+        >
+          <Dialog.Title>Alert</Dialog.Title>
+          <Dialog.Content>
+            <Text style={{ color: colors.text }}>
+              Successfully submitted, bro
+            </Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => setDialogOpen(false)}>Ok, big bro</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
     </SafeAreaWrapper>
   );
 }
@@ -19,7 +88,7 @@ const createStyles = (colors: any) =>
   StyleSheet.create({
     title: {
       fontSize: typography.xl,
-      color: "white",
+      color: colors.text,
       marginBottom: 64,
     },
   });
---
+++ components/RHF/customInput.tsx
@@ -0,0 +1,143 @@
+import { typography } from "@/constants/theme";
+import { useTheme } from "@react-navigation/native";
+import { useState } from "react";
+import {
+  Control,
+  Controller,
+  FieldValues,
+  RegisterOptions,
+} from "react-hook-form";
+import {
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
+import { EyeIcon, EyeSlashIcon } from "react-native-heroicons/solid";
+
+interface inputProps {
+  control: Control<any>;
+  name: string;
+  rules?:
+    | Omit<
+        RegisterOptions<FieldValues, string>,
+        "disabled" | "valueAsNumber" | "valueAsDate" | "setValueAs"
+      >
+    | undefined;
+  label?: string;
+  placeholder?: string;
+  isPassword?: boolean;
+  isEmail?: boolean;
+  style?: StyleProp<ViewStyle>;
+  required?: boolean;
+}
+export default function CustomInput({
+  control,
+  name,
+  rules,
+  label,
+  placeholder,
+  isPassword,
+  isEmail,
+  style,
+  required,
+}: inputProps) {
+  const { colors } = useTheme();
+  const styles = returnStyles(colors);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+  return (
+    <View style={style || {}}>
+      {label && <Text style={styles.label}>{label}</Text>}
+      <Controller
+        control={control}
+        name={name}
+        rules={{
+          required: required && "This field is required",
+
+          minLength: isPassword
+            ? {
+                value: 8,
+                message: "Passwords must be 8 characters or more",
+              }
+            : undefined,
+          validate: isEmail
+            ? (value) =>
+                /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(
+                  value
+                ) || "Please enter a valid email address"
+            : undefined,
+          ...rules,
+        }}
+        render={({
+          field: { value, onChange, onBlur },
+          fieldState: { error },
+        }) => (
+          <>
+            <View style={styles.textInputWrapper}>
+              <TextInput
+                style={styles.textInput}
+                placeholder={placeholder || ""}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                keyboardType={isEmail ? "email-address" : "default"}
+                secureTextEntry={isPassword && !isPasswordVisible}
+              />
+              {isPassword && (
+                <TouchableOpacity
+                  onPress={() => setIsPasswordVisible((prev) => !prev)}
+                >
+                  {isPasswordVisible ? (
+                    <EyeSlashIcon fill={colors.text} style={{ width: 24 }} />
+                  ) : (
+                    <EyeIcon fill={colors.text} style={{ width: 24 }} />
+                  )}
+                </TouchableOpacity>
+              )}
+            </View>
+
+            {error && <Text style={styles.errorText}>{error?.message}</Text>}
+          </>
+        )}
+      />
+    </View>
+  );
+}
+
+const returnStyles = (colors: any) =>
+  StyleSheet.create({
+    mainWrapper: {
+      gap: 8,
+    },
+    label: {
+      color: colors.textSecondary,
+      fontSize: typography.sm,
+      marginBottom: 4,
+    },
+    errorText: {
+      color: colors.error,
+      fontSize: typography.sm,
+    },
+    textInput: {
+      color: colors.text,
+      fontSize: typography.md,
+      width: "90%",
+    },
+    textInputWrapper: {
+      borderWidth: 2,
+      borderColor: colors.border,
+      borderRadius: 8,
+      paddingHorizontal: 8,
+      flexDirection: "row",
+      overflow: "hidden",
+      alignItems: "center",
+      width: "100%",
+    },
+  });
